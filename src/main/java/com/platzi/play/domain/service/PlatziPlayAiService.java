fa76daebb1db5132b3a04abf860d87cf9924fbf1
JeanCaicedo <jeanncaicedo@outlook.com>
2025-11-11 22:55:25 -0500

    Organizacion y Estructura

---
+++ src/main/java/com/platzi/play/domain/service/PlatziPlayAiService.java
@@ -0,0 +1,15 @@
+package com.platzi.play.domain.service;
+
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
+import dev.langchain4j.service.spring.AiService;
+
+@AiService
+public interface PlatziPlayAiService {
+
+    @UserMessage("""
+            Genera un saludo de bienvenida a la plataforma de Gestion de peliculas {{plataform}}.
+            Usa menos de 120 caracteres.
+            """)
+    String generateGreeting(@V("plataform")String plataform);
+}
